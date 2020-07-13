d30d24bee5207085e3843d71f331b06f42cdd469
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2020-07-14 00:17:51 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ContactListComponent } from './pages/contact-list/contact-list.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent,
   ],
   imports: [
     BrowserModule,
