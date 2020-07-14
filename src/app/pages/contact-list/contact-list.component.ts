12c4be89987b4c4808e64ec27ac5d7630a4d86a0
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2020-07-14 21:38:02 +0200

    push

--- src/app/pages/contact-list/contact-list.component.ts
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -8,23 +8,47 @@ import { NgAnimateScrollService } from 'ng-animate-scroll';
   styleUrls: ['./contact-list.component.scss']
 })
 export class ContactListComponent implements OnInit {
-  private _jsonURL = 'assets/contacts.json';
+  private _jsonURL = 'assets/rcontact.json';
   contents: any[] = [];
   letters: any[] = [];
-  
-  constructor(private http: HttpClient,private animateScrollService: NgAnimateScrollService) {
+  data: any[] = [];
+  searchItems: any[] = [];
+  constructor(private http: HttpClient, private animateScrollService: NgAnimateScrollService) {
 
     this.getJSON()
 
   }
   navigateToHeader(contentList) {
     this.animateScrollService.scrollToElement(contentList, 1000)
-}
+  }
   ngOnInit(): void {
+    console.log(this.searchItems.length)
+  }
+
+  searchBar(search) {
+    console.log(search.target.value)
+    if (search.target.value != '') {
+      this.searchItems = this.data.filter(e => {
+        let name = `${e.firstName.toLowerCase()}  ${e.lastName.toLowerCase()}`
+        return name.indexOf(search.target.value.toLowerCase()) > -1;
+      });
+    } else {
+      this.searchItems = [];
+    }
+
+
+
+
   }
   public getJSON() {
     this.http.get(this._jsonURL).subscribe((content: any) => {
-      content.data.forEach(e => {
+      // this.data = content.data;
+    this.data = content.data.sort(function(a, b){
+        if(a.firstName < b.firstName) { return -1; }
+        if(a.firstName > b.firstName) { return 1; }
+        return 0;
+    });
+      this.data.forEach(e => {
         e.firstName = e.firstName.charAt(0).toUpperCase() + e.firstName.slice(1);
         if (this.letters.indexOf(e.firstName.charAt(0)) == -1) {
           this.letters.push(e.firstName.charAt(0));
@@ -37,7 +61,7 @@ export class ContactListComponent implements OnInit {
         }
         this.contents[this.letters.length - 1].items.push(e)
       })
-      console.log( this.contents)
+      console.log(this.data)
     });
   }
 }
